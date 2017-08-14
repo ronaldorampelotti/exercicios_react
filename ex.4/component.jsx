05ae6d80f45650079612c58a38d6340f3a3b2a89
ronaldorampelotti <ronaldo.rampelotti@gmail.com>
2017-08-14 15:54:19 -0300

    Commit inicial.

---
+++ ex.4/component.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+// Restante código JavaScript
+
+export default props => (
+    <h1>Primeiro Componente ! </h1> // Código específico do React
+)
+
+export const Segundo = props => (
+    <h1>Segundo Componente !</h1> // Código específico do React
+)
+
+//export { Primeiro, Segundo }
+
